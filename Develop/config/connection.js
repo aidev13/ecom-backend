11128d24e82e562c01e35ac901858234313e07fb
David Liebherr <ambition.aidev@gmail.com>
2023-09-23 13:18:28 -0500

    WIP, defined all columns in models according to the seeds file provided

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -12,4 +12,11 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
+    try {
+      sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+
 module.exports = sequelize;
